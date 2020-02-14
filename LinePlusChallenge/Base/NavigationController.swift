37de9b3a2ebab78551f5c542384f11112001ccd2
Insu Park <facessu@gmail.com>
2020-02-14 16:33:38 +0900

    Base Navi

---
+++ LinePlusChallenge/Base/NavigationController.swift
@@ -0,0 +1,35 @@
+//
+//  NavigationController.swift
+//  LinePlusChallenge
+//
+//  Created by Insu Park on 2020/02/14.
+//  Copyright © 2020 INSWAG. All rights reserved.
+//
+
+import UIKit
+
+class NavigationController: UINavigationController {
+    
+    // MARK:- Initialize
+    
+    override init(rootViewController: UIViewController) {
+        super.init(rootViewController: rootViewController)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK:- View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUIComponents()
+    }
+    
+    // MARK:- Methods
+    func setupUIComponents() {
+        self.navigationBar.setValue(true, forKey: "hidesShadow")
+        self.navigationBar.barTintColor = UIColor.white
+    }
+    
+}
