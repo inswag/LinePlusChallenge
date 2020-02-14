8bfbd49fff60b88259bc2a3af0a1d811921c2582
Insu Park <facessu@gmail.com>
2020-02-14 16:31:35 +0900

    Initial Navigator Setting

---
+++ LinePlusChallenge/Core/Navigator.swift
@@ -0,0 +1,26 @@
+//
+//  Navigator.swift
+//  LinePlusChallenge
+//
+//  Created by Insu Park on 2020/02/14.
+//  Copyright © 2020 INSWAG. All rights reserved.
+//
+
+import UIKit
+
+class Navigator {
+    
+    enum Scene {
+        case memoList
+    }
+    
+    func get(segue: Scene) -> UIViewController {
+        switch segue {
+        case .memoList:
+            let memoListVC = MemoListViewController()
+            return memoListVC
+        }
+    }
+    
+    
+}
